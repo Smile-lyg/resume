4569dd1c4609482bc4ec46d2a480b5a8671aabfd
itsmile <1921243831@qq.com>
2024-10-09 00:41:16 +0800

    doc：更新简历信息

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,14 @@ function myloading() {
     document.getElementsByClassName('avatar')[0].style.display = 'block';
     document.getElementsByClassName('loading')[0].style.display = 'none';
 }
+
+function showPic(fname) {
+    document.querySelector('#dialogBg').style.display = 'block'
+    document.querySelector('#dialogBg>.dialog>.certImg').setAttribute('src', `./assets/images/${fname}.jpg`)
+}
+
+window.onload = function () {
+    document.querySelector('#dialogBg').addEventListener('click', function () {
+        this.style.display = 'none'
+    })
+}
